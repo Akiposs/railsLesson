83de263dd5eca775bd2f881c26f8436682779434
Akihiro_Takano <superakip1995@gmail.com>
2020-03-29 19:31:32 +0900

    init

---
+++ app/controllers/file_manage_controller.rb
@@ -0,0 +1,24 @@
+class FileManageController < ApplicationController
+
+  #ファイルアップロード画面用GETアクション
+  def uploadFile
+  end
+
+  #ファイルアップロード用POSTアクション
+  def uploadFileExecution
+  end
+
+
+  #ファイル一覧画面用GETアクション
+  def listFile
+  end
+
+  #ファイル内容詳細画面用GETアクション
+  def showFile
+  end
+
+  #ファイル削除用POSTアクション
+  def deleteFile
+  end
+
+end
---
+++ app/helpers/file_manage_helper.rb
@@ -0,0 +1,2 @@
+module FileManageHelper
+end
