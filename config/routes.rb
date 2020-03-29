3263e0fc40e85c4db0212199d8228c81620cfb9a
Akihiro_Takano <superakip1995@gmail.com>
2020-03-29 20:16:32 +0900

    route修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get  "/" => "fileManage#listFile"
-  get  "/uploadFile" => "fileManage#listFile"
-  post "/uploadFileExecution" => "fileManage#uploadFileExecution"
-  get  "/showFile" => "fileManage#showFile"
-  post "/deleteFile" => "fileManage#deleteFile"
+  root 'file_manage#listFile'
+  get  "/uploadFile",             to: 'file_manage#listFile'
+  post "/uploadFileExecution",    to: 'file_manage#uploadFileExecution'
+  get  "/showFile",               to: 'file_manage#showFile'
+  post "/deleteFile",             to: 'file_manage#deleteFile'
 end
